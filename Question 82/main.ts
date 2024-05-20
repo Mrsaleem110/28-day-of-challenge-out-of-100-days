75caf33fd54d33979c14a6fd80d613df52f61aa9
muhammad_saleem <160356353+Mrsaleem110@users.noreply.github.com>
2024-05-21 01:45:49 +0500

    adding 28 day of challenge

---
+++ Question 82/main.ts
@@ -0,0 +1,10 @@
+// *Question 82:* Find the Length of a String: Write a function that takes a string as an argument and returns the number of characters in the string.
+
+// *Explain & TIP:* The .length property of a string gives you the total number of characters it contains, helping you understand more about the data you're working with.
+
+function stringLength(str: string): number {
+    return str.length; // Returns the number of characters in the string
+}
+
+// Example: Measuring the length of a name
+console.log(stringLength("Alice"));
